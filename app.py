5c78e66c24b16992d6fd4e106a6331bae74e97a1
刘晓桐 <liuxiaotong@karina-3.local>
2026-04-21 22:58:04 +0800

    Add login and signup pages (Closes #6 #7)

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route('/chat')
 def chat():
-    return render_template('chat.html')
+    return "Chat page"
+
+@app.route('/skills')
+def skills():
+    return "Skills page"
+
+@app.route('/requests')
+def requests():
+    return "Requests page"
 
 @app.route('/profile')
 def profile():
-    return render_template('profile.html')
+    return "Profile page"
+
+@app.route('/logout')
+def logout():
+    return "Logout"
+
+if __name__ == '__main__':
+    app.run(debug=True)

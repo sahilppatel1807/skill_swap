61d62b4816d061fbcf066af1551a9b9303d14ae9
刘晓桐 <liuxiaotong@karina-3.local>
2026-05-06 12:35:17 +0800

    feat: add database-backed authentication

---
+++ models.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+
+db = SQLAlchemy()
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+
+    id         = db.Column(db.Integer, primary_key=True)
+    name       = db.Column(db.String(100), nullable=False)
+    email      = db.Column(db.String(150), unique=True, nullable=False)
+    password   = db.Column(db.String(256), nullable=False)
+    course     = db.Column(db.String(150))
+    bio        = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def set_password(self, raw_password):
+        self.password = generate_password_hash(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password_hash(self.password, raw_password)
+
+    def __repr__(self):
+        return f'<User {self.email}>'

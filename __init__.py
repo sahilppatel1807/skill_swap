97f56d42afe4547d02dc58b1ef95f44f1e282d6c
Karinaa-Liu <24336969@student.uwa.edu.au>
2026-05-11 19:19:29 +0800

    feat: add Flask-WTF and CSRF protection #89

--- __init__.py
+++ __init__.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_wtf import CSRFProtect
 from config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
+csrf = CSRFProtect()
 
 def create_app():
-	app = Flask(__name__)
-	app.config.from_object(Config)
-	db.init_app(app)
-	migrate.init_app(app, db)
-	return app
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    csrf.init_app(app)
+
+    return app
